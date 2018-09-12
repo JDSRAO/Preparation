4653cd1070c8a29e58c8b296a2483a5e2bf57e04
jdsrao <kjdssrinivasarao@gmail.com>
2018-09-12 22:02:12 +0530

    Modified program to show user readable output

--- CSharpSamples/DSAndAlgorithms/Algorithms/AlgorithmProgram.cs
+++ CSharpSamples/DSAndAlgorithms/Algorithms/AlgorithmProgram.cs
@@ -11,6 +11,7 @@ namespace CSharpSamples.DSAndAlgorithms.Algorithms
     {
         public void Run()
         {
+            // Simple sort
             List<int> numbers = new List<int>()
             {
                 1243,
@@ -18,8 +19,14 @@ namespace CSharpSamples.DSAndAlgorithms.Algorithms
                 24,
                 123,
                 2
-            };            
+            };
+            Console.WriteLine("\t Before Simple Sorting");
+            numbers.Print();
+            numbers.SimpleSort();
+            numbers.Print();
+            Console.WriteLine("\t After simple Sorting");
 
+            // Quick sort
             List<int> numbers2 = new List<int>()
             {
                89,
@@ -30,21 +37,84 @@ namespace CSharpSamples.DSAndAlgorithms.Algorithms
                99,
                12
             };
-            Console.WriteLine("Before Sorting");
+            Console.WriteLine("Before quick Sorting");
             numbers2.Print();
-            numbers.Contains(2);
-            //numbers.SimpleSort();
-            //numbers.SimpleSortOptimised();
-            //numbers.MergeSort();
-            //numbers.BubbleSort();
-            //numbers.BubbleSortOptimised();
             numbers2.QuickSort();
+            numbers2.Print();
+            Console.WriteLine("After quick Sorting");
+
+            // Simple sort optimised
+            List<int> numbers3 = new List<int>()
+            {
+               89,
+               76,
+               92,
+               67,
+               45,
+               99,
+               12
+            };
+            Console.WriteLine("Before simple sort optimised Sorting");
+            numbers3.Print();
+            numbers3.SimpleSortOptimised();
+            numbers3.Print();
+            Console.WriteLine("After simple sort optimised Sorting");
+
+            // Merge sort
+            List<int> numbers4 = new List<int>()
+            {
+               89,
+               76,
+               92,
+               67,
+               45,
+               99,
+               12
+            };
+            Console.WriteLine("Before merge Sorting");
+            numbers4.Print();
+            numbers4.MergeSort();
+            numbers4.Print();
+            Console.WriteLine("After merge Sorting");
+
+            // Bubble sort optimised
+            List<int> numbers5 = new List<int>()
+            {
+               89,
+               76,
+               92,
+               67,
+               45,
+               99,
+               12
+            };
+            Console.WriteLine("Before bubble sort optimsied");
+            numbers5.Print();
+            numbers5.BubbleSortOptimised();
+            numbers5.Print();
+            Console.WriteLine("After bubble sort optimsied");
+
+            // Bubble sort 
+            List<int> numbers6 = new List<int>()
+            {
+               89,
+               76,
+               92,
+               67,
+               45,
+               99,
+               12
+            };
+            Console.WriteLine("Before bubble sort");
+            numbers6.Print();
+            numbers6.BubbleSortOptimised();
+            numbers6.Print();
+            Console.WriteLine("After bubble sort");
+
             //numbers.InsertionSort();
             //numbers.InsertionSortBookAlgo();
             //numbers2.ShellSort();
-            //numbers.Slide(1243, 0, 3);
-            Console.WriteLine("After bubble Sorting");
-            numbers2.Print();
+            //numbers.Slide(1243, 0, 3);            
 
             List<string> stringList = new List<string>()
             {
