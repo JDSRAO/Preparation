8703cd4098feb58895c1cfc661656840a91d369f
jdsrao <kjdssrinivasarao@gmail.com>
2018-10-07 00:02:15 +0530

    Identity and Code first setup

---
+++ Vidly/Identity/AuthContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Identity.Models;
+
+namespace Vidly.Identity
+{
+    public class AuthContext : IdentityDbContext<ApplicationUser>
+    {
+        public AuthContext() : base("authContext")
+        {
+
+        }
+    }
+}
