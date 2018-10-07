6e6eb109913c7143deac2556f7d5c95b5d8f07a2
jdsrao <kjdssrinivasarao@gmail.com>
2018-10-07 16:38:46 +0530

    Fetching movie data from DB

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Identity;
 using Vidly.Models;
 
 namespace Vidly.Controllers
@@ -10,23 +11,18 @@ namespace Vidly.Controllers
     [RoutePrefix("movies")]
     public class CustomersController : Controller
     {
-        static List<Customer>  customers = new List<Customer>()
-        {
-            new Customer() { ID = 1, Name = "KJDS" },
-            new Customer() { ID = 2, Name = "RDJ" },
-            new Customer() { ID = 3, Name = "Captain America" },
-        };
-
+        private AuthContext context = new AuthContext();
         // GET: Customers
         public ActionResult Index()
         {
+            var customers = context.Customers.ToList();
             return View(customers);
         }
 
         [Route("details/{id}")]
         public ActionResult Details(int id)
         {
-            var customer = customers.FirstOrDefault(x => x.ID == id);
+            var customer = context.Customers.FirstOrDefault(x => x.ID == id);
             if(customer == null)
             {
                 return HttpNotFound();
