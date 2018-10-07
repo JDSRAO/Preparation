321c2aac6726f48da3e6768e8018d1f1ab077870
jdsrao <kjdssrinivasarao@gmail.com>
2018-10-07 17:04:51 +0530

    Section 3 : Exercise - 1

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,7 +16,7 @@ namespace Vidly.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = context.Customers.ToList();
+            var customers = context.Customers.Include(x => x.MembershipType).ToList();
             return View(customers);
         }
 
