3fdba46706c31aa6acf45e34013b568400074747
jdsrao <kjdssrinivasarao@gmail.com>
2018-10-08 18:50:35 +0530

    Web api JSON formatter specification

--- Vidly/App_Start/WebApiConfig.cs
+++ Vidly/App_Start/WebApiConfig.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using Vidly.Filters;
 
 namespace Vidly
 {
@@ -10,7 +13,7 @@ namespace Vidly
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config = ConfigureWebApi();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -19,6 +22,31 @@ namespace Vidly
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+
+        /// <summary>
+        /// Configures the WebAPI
+        /// </summary>
+        /// <returns>HttpConfiguration Object populated with default configuration values</returns>
+        private static HttpConfiguration ConfigureWebApi()
+        {
+            //Configuration objetct
+            HttpConfiguration config = new HttpConfiguration();
+
+            //Error detail policy
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+
+            config.Filters.Add(new HandleExceptionAttribute());
+            //Map routes
+            config.MapHttpAttributeRoutes();
+
+            //Formatter for json
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            //Added for Entity Frameowrk Reference Loop issues
+            jsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            return config;
         }
     }
 }
