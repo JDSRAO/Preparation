04712b4cbc07092d45be975e8e59e5006d83fb8c
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2022-06-24 12:23:05 +0530

    save commit

--- CSharpSamples/CodeFirstApproach/Configuration/UserProfileMap.cs
+++ CSharpSamples/CodeFirstApproach/Configuration/UserProfileMap.cs
@@ -10,33 +10,35 @@ namespace CSharpSamples.CodeFirstApproach.Configuration
 {
     public class UserProfileMap : EntityTypeConfiguration<UserProfile>
     {
-        public UserProfileMap()
-        {
-            //key  
-            HasKey(t => t.ID);
+        //public UserProfileMap()
+        //{
+        //    //key  
+        //    HasKey(t => t.ID);
 
-            //fieds  
-            Property(t => t.FirstName);
-            Property(t => t.LastName);
-            Property(t => t.Address).HasMaxLength(100).HasColumnType("nvarchar");
-            Property(t => t.AddedDate);
-            Property(t => t.ModifiedDate);
-            Property(t => t.IP);
+        //    //fieds  
+        //    Property(t => t.FirstName);
+        //    Property(t => t.LastName);
+        //    Property(t => t.Address).HasMaxLength(100).HasColumnType("nvarchar");
+        //    Property(t => t.AddedDate);
+        //    Property(t => t.ModifiedDate);
+        //    Property(t => t.IP);
 
-            //table  
-            ToTable("UserProfiles");
+        //    HasIndex(x => x.AddedDate)
 
-            //one to one relationship  
-            HasRequired(t => t.User).WithRequiredDependent(u => u.UserProfile);
+        //    //table  
+        //    ToTable("UserProfiles");
 
-            //one to many relationship  
-            //HasRequired(t => t.Customer).WithMany(c => c.Orders).HasForeignKey(t => t.CustomerId).WillCascadeOnDelete(false);
+        //    //one to one relationship  
+        //    HasRequired(t => t.User).WithRequiredDependent(u => u.UserProfile);
 
-            //Many to many relationship  
-            //HasMany(t => t.Courses).WithMany(c => c.Students)
-            //                     .Map(t => t.ToTable("StudentCourse")
-            //                         .MapLeftKey("StudentId")
-            //                         .MapRightKey("CourseId"));
-        }
+        //    //one to many relationship  
+        //    //HasRequired(t => t.Customer).WithMany(c => c.Orders).HasForeignKey(t => t.CustomerId).WillCascadeOnDelete(false);
+
+        //    //Many to many relationship  
+        //    //HasMany(t => t.Courses).WithMany(c => c.Students)
+        //    //                     .Map(t => t.ToTable("StudentCourse")
+        //    //                         .MapLeftKey("StudentId")
+        //    //                         .MapRightKey("CourseId"));
+        //}
     }
 }
